83029b46ee43a0151d81202a9c6abac1e32e29e7
BeatDiaz <beatdiazmusic@gmail.com>
2025-08-28 13:00:26 +0300

    refactor: update MoodBtn and MoodSelector to use defined Mood type and improve state management

--- src/comps/MoodBtn.tsx
+++ src/comps/MoodBtn.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+type Mood = "Neutral 😐" | "Happy 😀" | "Sad 😢" | "Angry 😡";
+
 type Props = {
-    label: string,
-    mood: string,
-    onClick: (mood: string) => void;
+  label: string,
+  mood: Mood,
+  onClick: (mood: Mood) => void;
 }
+
 export default function MoodBtn({label, mood, onClick}: Props) {
+  const [count, setCount] = useState(0);
   return (
-    <button onClick={() => {onClick(mood)}}>{label}</button>
+    <button onClick={() => {onClick(mood); setCount(count + 1);} }>{`${label}, ${count}`}</button>
   )
 }
 
--- src/comps/MoodSelector.tsx
+++ src/comps/MoodSelector.tsx
@@ -2,22 +2,52 @@ import { useState } from "react";
 import MoodBtn from "./MoodBtn";
 
 const moods = {
-  neutral: "Neutral 😐",
   happy: "Happy 😀",
   sad: "Sad 😢",
   angry: "Angry 😡"
- };
+ } as const;
+
+ type Mood = "Neutral 😐" | "Happy 😀" | "Sad 😢" | "Angry 😡";
 
 export default function MoodSelector() {
-  const [currentMood, setCurrentMood] = useState(moods.neutral);
+  const [currentMood, setCurrentMood] = useState<Mood>("Neutral 😐");
+  const [history, setHistory] = useState<Mood[]>([]);
+  const [count, setCount] = useState([0,0,0]);
+
+  function handleSet(newMood: Mood) {
+    setCurrentMood(newMood);
+    setHistory((prev) => [...prev, newMood].slice(-3));
+  }
+
+  function randMood() {
+    const moodValues = Object.values(moods);
+    const x = Math.floor(Math.random() * 3);
+    const randomMood = moodValues[x] as Mood;
+    handleSet(randomMood);
+  }
+
+  function resetMoods() {
+    const moodValues = Object.values(moods);
+    moodValues.forEach((m) => {setHistory([]); setCurrentMood("Neutral 😐")})
+  }
+  
   return (
     <div>
         <h1>Mood Selector</h1>
       <h2>Current Mood: {currentMood}</h2>
       <div>
-        <MoodBtn label={moods.happy} mood={moods.happy} onClick={setCurrentMood} />
-        <MoodBtn label={moods.sad} mood={moods.sad} onClick={setCurrentMood} />
-        <MoodBtn label={moods.angry} mood={moods.angry} onClick={setCurrentMood} />
+        <MoodBtn label={moods.happy} mood={moods.happy} onClick={handleSet} />
+        <MoodBtn label={moods.sad} mood={moods.sad} onClick={handleSet} />
+        <MoodBtn label={moods.angry} mood={moods.angry} onClick={handleSet} />
+      </div>
+      <div>
+        <button onClick={randMood}>Random Mood</button>
+      </div>
+      <div>
+        <h3>History:</h3>
+          {history.map((m, i) => (
+            <h4 key={i}>{m}</h4>
+          ))}
       </div>
     </div>
   );
